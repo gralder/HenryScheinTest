e1590b1492e1bf223489b16308db8442b9c0ceb9
Glen Alder <glenalder@comcast.net>
2019-04-07 19:26:04 -0600

    Add project files.

---
+++ HenryScheinTestForms/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace HenryScheinTestForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Parse String";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(101, 28);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxInput.Size = new System.Drawing.Size(550, 130);
+            this.textBoxInput.TabIndex = 4;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(101, 233);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOutput.Size = new System.Drawing.Size(550, 130);
+            this.textBoxOutput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Input String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output String";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HenryScheinTestForms/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HenryScheinTestForms
+{
+    /*
+     * Test program: C# app that takes a string of CSV data as input and 
+     * returns the data reformatted as shown below.
+     *
+     * Example input string:
+     *   “Patient Name”,”SSN”,”Age”,”Phone Number”,”Status”
+     *   “Prescott, Zeke”,”542-51-6641”,21,”801-555-2134”,”Opratory=2,PCP=1”
+     *   “Goldstein, Bucky”,”635-45-1254”,42,”435-555-1541”,”Opratory=1,PCP=1”
+     *   “Vox, Bono”,”414-45-1475”,51,”801-555-2100”,”Opratory=3,PCP=2”
+     *
+
+     * Return string for the above sample input 
+     *   [Patient Name] [SSN] [Age] [Phone Number] [Status]
+     *   [Prescott, Zeke] [542-51-6641] [21] [801-555-2134] [Opratory=2,PCP=1]
+     *   [Goldstein, Bucky] [635-45-1254] [42] [435-555-1541] [Opratory=1,PCP=1]
+     *   [Vox, Bono] [414-45-1475] [51] [801-555-2100] [Opratory=3,PCP=2]
+     *
+     * The system should function with both \r\n and \n carriage return, new line
+     * configurations.
+     *
+     * Programmer:  Glen R. Alder
+     * Date:        4/6/2019
+     * 
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+            
+            textBoxOutput.Text = ParseString.ParseInputString(input);
+        }
+
+
+        //====================================================================
+        //                 METHODS
+        //====================================================================
+
+
+        /*
+         * This method will take in a CSV input string and return a formated string.
+         * 
+         * input: string  (“Patient Name”,”SSN”,”Age”,”Phone Number”,”Status”
+         *       “Prescott, Zeke”,”542-51-6641”,21,”801-555-2134”,”Opratory=2,PCP=1”)
+         *
+         * return: string   ([Patient Name] [SSN] [Age] [Phone Number] [Status]
+         *      [Prescott, Zeke] [542-51-6641] [21] [801-555-2134] [Opratory=2,PCP=1]
+         *      
+         */
+        static string xxParseInputString(string input)
+        {
+            string[] lines;
+            string[] words;
+            string word;
+            List<string> stringList = new List<string>();
+            string output = "";
+            string[] newWords;
+            string tempStr;
+
+            // break up input string too handle \n or \r\n
+            lines = input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            // loop through all lines 
+            foreach (string line in lines)
+            {
+                words = null;
+                // split line into words "“Patient Name"
+                words = line.Split('”');
+                stringList = new List<string>();
+
+                // loop through the words removing null and ',' 
+                foreach (string wordT in words)
+                {
+                    word = wordT.Replace("“", "");
+                    if (!string.IsNullOrEmpty(word) && word.Length > 1)
+                    {
+                        if (word[0].Equals(',')) // check for age ',99,'
+                        {
+                            tempStr = word.Replace(",", "");
+                            stringList.Add(tempStr.Trim());
+                        }
+                        else
+                        {
+                            stringList.Add(word.Trim());
+                        }
+                    }
+                }
+
+                newWords = stringList.ToArray();
+
+                // append line to return string
+                foreach (var wordT in newWords)
+                {
+                    output = output + $"[{wordT}] ";
+                }
+                output = output + "\r\n";
+            }
+            return output;
+        }
+    }
+}
