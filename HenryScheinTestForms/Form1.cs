c1b63886f824e36d2f012eb1c6d039ba833c0678
Glen Alder <glenalder@comcast.net>
2019-04-07 20:59:55 -0600

    test

--- HenryScheinTestForms/Form1.cs
+++ HenryScheinTestForms/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 namespace HenryScheinTestForms
 {
+
     /*
      * Test program: C# app that takes a string of CSV data as input and 
      * returns the data reformatted as shown below.
@@ -50,70 +51,5 @@ namespace HenryScheinTestForms
         }
 
 
-        //====================================================================
-        //                 METHODS
-        //====================================================================
-
-
-        /*
-         * This method will take in a CSV input string and return a formated string.
-         * 
-         * input: string  (“Patient Name”,”SSN”,”Age”,”Phone Number”,”Status”
-         *       “Prescott, Zeke”,”542-51-6641”,21,”801-555-2134”,”Opratory=2,PCP=1”)
-         *
-         * return: string   ([Patient Name] [SSN] [Age] [Phone Number] [Status]
-         *      [Prescott, Zeke] [542-51-6641] [21] [801-555-2134] [Opratory=2,PCP=1]
-         *      
-         */
-        static string xxParseInputString(string input)
-        {
-            string[] lines;
-            string[] words;
-            string word;
-            List<string> stringList = new List<string>();
-            string output = "";
-            string[] newWords;
-            string tempStr;
-
-            // break up input string too handle \n or \r\n
-            lines = input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-            // loop through all lines 
-            foreach (string line in lines)
-            {
-                words = null;
-                // split line into words "“Patient Name"
-                words = line.Split('”');
-                stringList = new List<string>();
-
-                // loop through the words removing null and ',' 
-                foreach (string wordT in words)
-                {
-                    word = wordT.Replace("“", "");
-                    if (!string.IsNullOrEmpty(word) && word.Length > 1)
-                    {
-                        if (word[0].Equals(',')) // check for age ',99,'
-                        {
-                            tempStr = word.Replace(",", "");
-                            stringList.Add(tempStr.Trim());
-                        }
-                        else
-                        {
-                            stringList.Add(word.Trim());
-                        }
-                    }
-                }
-
-                newWords = stringList.ToArray();
-
-                // append line to return string
-                foreach (var wordT in newWords)
-                {
-                    output = output + $"[{wordT}] ";
-                }
-                output = output + "\r\n";
-            }
-            return output;
-        }
     }
 }
